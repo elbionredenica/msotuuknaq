2cd1d0b87f3089cbeb94ff3a7bf529ebfd7a96be
nyctamentlaugh <nyctamentlaugh@gmail.com>
2021-09-05 12:53:42 +0200

    push to heroku

--- public/authentication/main.js
+++ public/authentication/main.js
@@ -9,7 +9,7 @@ if (user !== null) {
   const randomFromUid = uid.split("");
   console.log(displayName, phone, uid);
   var temp_nickname = 'Guest' + Math.floor(Math.random()*90000) + randomFromUid[27] + randomFromUid[7];
-    
+  console.log(uid)
 
     if (displayName !== null) {
       document.getElementById('name').value = displayName;
