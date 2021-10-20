e53d870c204dbeb9670104b79c5e0f9284e0aed9
nyctamentlaugh <nyctamentlaugh@gmail.com>
2021-10-20 15:18:57 +0200

    Push to git

--- public/js/create.js
+++ public/js/create.js
@@ -5,18 +5,30 @@ socket.on('connect', function(){
 });
 
 socket.on('gameNamesData', function(data){
-    for(var i = 0; i < Object.keys(data).length; i++){
-        var div = document.getElementById('game-list');
-        var button = document.createElement('button');
-        
-        button.innerHTML = data[i].name;
-        button.setAttribute('onClick', "startGame('" + data[i].id + "')");
-        button.setAttribute('id', 'gameButton');
-        
-        div.appendChild(button);
-        div.appendChild(document.createElement('br'));
-        div.appendChild(document.createElement('br'));
-    }
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log(user.uid)
+            for(var i = 0; i < Object.keys(data).length; i++){
+                if (user.uid == data[i].userID) {
+                    
+                    var div = document.getElementById('game-list');
+                    var button = document.createElement('button');
+                
+                    button.innerHTML = data[i].name;
+                    button.setAttribute('onClick', "startGame('" + data[i].id + "')");
+                    button.setAttribute('id', 'gameButton');
+                
+                    div.appendChild(button);
+                    div.appendChild(document.createElement('br'));
+                    div.appendChild(document.createElement('br'));
+                } else {
+                    console.log("test")
+                }
+                
+            }
+        }
+    });
+    
 });
 
 function startGame(data){
--- public/js/quizCreator.js
+++ public/js/quizCreator.js
@@ -7,21 +7,30 @@ function updateDatabase(){
             alert("Ju lutem plotësoni të gjitha të gjitha të dhënat!");
             return false;
         } else {
-            var questions = [];
-            var name = document.getElementById('name').value;
-            for(var i = 1; i <= questionNum; i++){
-                var question = document.getElementById('q' + i).value;
-                var answer1 = document.getElementById(i + 'a1').value;
-                var answer2 = document.getElementById(i + 'a2').value;
-                var answer3 = document.getElementById(i + 'a3').value;
-                var answer4 = document.getElementById(i + 'a4').value;
-                var correct = document.getElementById('correct' + i).value;
-                var answers = [answer1, answer2, answer3, answer4];
-                questions.push({"question": question, "answers": answers, "correct": correct})
+            var userID = null;
+            firebase.auth().onAuthStateChanged((user) => { 
+                if (user) {
+                    userID = user.uid;
+                    var questions = [];
+                    var name = document.getElementById('name').value;
+                    for(var i = 1; i <= questionNum; i++){
+                        var question = document.getElementById('q' + i).value;
+                        var answer1 = document.getElementById(i + 'a1').value;
+                        var answer2 = document.getElementById(i + 'a2').value;
+                        var answer3 = document.getElementById(i + 'a3').value;
+                        var answer4 = document.getElementById(i + 'a4').value;
+                        var correct = document.getElementById('correct' + i).value;
+                        var answers = [answer1, answer2, answer3, answer4];
+                        questions.push({"question": question, "answers": answers, "correct": correct})
             }
     
-            var quiz = {id: 0, "name": name, "questions": questions};
-            socket.emit('newQuiz', quiz);
+                    var quiz = {id: 0, "name": name, "questions": questions, "userID": userID};
+                    socket.emit('newQuiz', quiz);
+                } else {
+                    console.log("pain")
+                }
+            });
+            
         }
 }
 
